b07d4ef227fe1ad2efc09c9673928c26a1e9a212
Philipp Koch <lampyridae2000@gmail.com>
2020-03-31 18:33:18 +0200

    Add Readme, Change HASHTAG_LIST location

---
+++ hashtags.py
@@ -0,0 +1,5 @@
+HASHTAG_LIST = [
+    "covid_19", "coronavirus", "virus", "corona", "chineseviruscorona", "chinesevirus",
+    "Covid_19", "Coronavirus", "Virus", "Corona", "ChineseVirusCorona", "ChineseVirus",
+    "CoronaVirus", "Chinesevirus"
+    ]
